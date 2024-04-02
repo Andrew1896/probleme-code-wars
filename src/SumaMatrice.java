97f12737a41c16be997c7b558f8b879c4c1837ce
Andrei Cataraga <andreicataraga18@gmail.com>
2024-04-02 23:38:50 +0200

    SumaMatrice

---
+++ src/SumaMatrice.java
@@ -0,0 +1,24 @@
+public class SumaMatrice {
+        public static double sumaMatrice(double[][] matrice) {
+            double suma = 0;
+            if (matrice == null || matrice.length == 0 || matrice[0].length == 0) {
+                return 0;
+            }
+
+            for (int i = 0; i < matrice.length; i++) {
+                for (int j = 0; j < matrice[i].length; j++) {
+                    suma += matrice[i][j];
+                }
+            }
+            return suma;
+        }
+
+        public static void main(String[] args) {
+            double[][] matriceExemplu = {
+                    {2, 6, 3.7, -13, 21}
+            };
+            double rezultat = sumaMatrice(matriceExemplu);
+            System.out.println("Suma numerelor din matrice este: " + rezultat);
+        }
+    }
+
