a9d72205e17f8fa95560411150517ef7a61562b3
Andrei Cataraga <andreicataraga18@gmail.com>
2024-04-18 22:11:14 +0200

    WellOfIdeas

---
+++ src/WellOfIdeas.java
@@ -0,0 +1,24 @@
+public class WellOfIdeas {
+
+    public static String checkIdea(String[] x) {
+        int goodIdea = 0;
+        for (String idea : x) {
+            if (idea.equals("good")) {
+                goodIdea++;
+            }
+        }
+        if (goodIdea == 1 || goodIdea == 2) {
+            return "Publish!";
+        } else if (goodIdea > 2) {
+            return "I smell a series!";
+        } else {
+            return "Fail!";
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] idea = {"good", "bad", "bad", "good"};
+        String result = checkIdea(idea);
+        System.out.println(result);
+    }
+}
