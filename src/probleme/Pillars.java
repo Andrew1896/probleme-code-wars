4cdc6fbab6a5857c26241ab6ac79c90104323e40
Andrei Cataraga <andreicataraga18@gmail.com>
2024-04-24 00:12:26 +0200

    Update Pillars

--- src/probleme/Pillars.java
+++ src/probleme/Pillars.java
@@ -3,14 +3,14 @@ package probleme;
 public class Pillars {
 
     public static int distanceBetweenPillars(int n, int d, int w) {
-        int distanceBetweenPillars = ((n - 1) * d * 100) - (2 * w);
+        int distanceBetweenPillars = ((n - 1) * d * 100) + (w*(n-2));
         return distanceBetweenPillars;
     }
 
     public static void main(String[] args) {
-        int n = 5 ;
-        int d = 10;
-        int w = 30;
+        int n = 5;
+        int d = 20;
+        int w = 20;
 
         int result = distanceBetweenPillars(n, d, w);
         System.out.println("Distanța dintre primul și ultimul pilon: " + result + " centimetri");
