ddb3e39a694ffb34c35ac4fb1fa2bf5bba6c6572
Andrei Cataraga <andreicataraga18@gmail.com>
2024-04-25 23:27:04 +0200

    CombineNames

---
+++ src/probleme/CombineNames.java
@@ -0,0 +1,11 @@
+package probleme;
+
+public class CombineNames {
+
+        public static void main(String[]args){
+            String name = "Andrei";
+            String surname = " Cataraga";
+            System.out.println(name + surname);
+        }
+    }
+
